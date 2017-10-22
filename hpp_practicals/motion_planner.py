3a93562bb6744659e1a7acda5f10e8b15856242e
Naarukarmic <pierre.lemarquand@gmail.com>
2017-10-22 18:51:03 +0200

    Motion planner

--- hpp_practicals/motion_planner.py
+++ hpp_practicals/motion_planner.py
@@ -1,50 +1,59 @@
 class MotionPlanner:
-  def __init__ (self, robot, ps):
-    self.robot = robot
-    self.ps = ps
+	def __init__ (self, robot, ps):
+		self.robot = robot
+		self.ps = ps
 
-  def solveBiRRT (self, maxIter = float("inf")):
-    # print ("Method solveBiRRT is not implemented yet")
-    self.ps.prepareSolveStepByStep ()
-    finished = False
+	def solveBiRRT (self, maxIter = float("inf")):
+		deltaQ = 0.01
+		self.ps.prepareSolveStepByStep()
+		finished = False
 
-    # In the framework of the course, we restrict ourselves to 2 connected components.
-    nbCC = self.ps.numberConnectedComponents ()
-    if nbCC != 2:
-      raise Exception ("There should be 2 connected components.")
+		# In the framework of the course, we restrict ourselves to 2 connected components.
+		nbCC = self.ps.numberConnectedComponents()
+		if nbCC != 2:
+			raise Exception("There should be 2 connected components.")
 
-    iter = 0
-    while True:
-      #### RRT begin
-      newConfigs = list ()
-      newConfigs.append(self.robot.shootRandomConfig())
+		iter = 0
+		while True:
+			#### RRT begin
+			newConfigs = list()
+			#Generate a random config 
+			q_rand = self.robot.shootRandomConfig()
+			#For each CC, find the nearest config 
+			q_near = list()
+			q_new = list()
+			for i in range(nbCC):
+				q_near.append(self.ps.getNearestConfig(q_rand,i)[0])
+				path = self.ps.directPath(q_near[i], q_rand, False)
+				q_new.append(self.ps.configAtParam(path[1],deltaQ))
+				#Test if new config is valid
+				link = self.ps.directPath(q_near[i],q_new[i], True)
+				if(link[0]):
+					self.ps.addConfigToRoadmap(q_new[i])
+					self.ps.addEdgeToRoadmap(q_near[i], q_new[i], link[1], True)
+				#Add q_new to newConfig
+				newConfigs.append(q_new[i])
+		
+			## Try connecting the new nodes together
+			connectLink = self.ps.directPath(newConfigs[0], newConfigs[1], True)
+			if(connectLink[0]):
+				self.ps.addEdgeToRoadmap(newConfigs[0], newConfigs[1],connectLink[1], True)
+			#### RRT end
+			## Check if the problem is solved.
+			nbCC = self.ps.numberConnectedComponents()
+			if nbCC == 1:
+				# Problem solved
+				finished = True
+				break
+			iter = iter + 1
+			if iter > maxIter:
+				break
+		if finished:
+				self.ps.finishSolveStepByStep()
+				return self.ps.numberPaths() - 1
 
-      neigh, dist = self.ps.getNearestConfig(newConfigs[0])
-      ## Try connecting the new nodes together
-      # for i in range (len(newConfigs) - 1):
-      #  print self.ps.directPath(newConfigs[i], newConfigs[len(newConfigs) - 1], True)
-      res = self.ps.directPath(neigh, newConfigs[0], True)
-      if res[0]:
-        print res
-        self.ps.addConfigToRoadmap(newConfigs[0])
-        self.ps.addEdgeToRoadmap(neigh, newConfigs[0], iter, True)
-
-      #### RRT end
-      ## Check if the problem is solved.
-      nbCC = self.ps.numberConnectedComponents ()
-      if nbCC == 1:
-        # Problem solved
-        finished = True
-        break
-      iter = iter + 1
-      if iter > maxIter:
-        break
-    if finished:
-        self.ps.finishSolveStepByStep ()
-        return self.ps.numberPaths () - 1
-
-  def solvePRM (self):
-    self.ps.prepareSolveStepByStep ()
-    #### PRM begin
-    #### PRM end
-    self.ps.finishSolveStepByStep ()
+	def solvePRM (self):
+		self.ps.prepareSolveStepByStep()
+		#### PRM begin
+		#### PRM end
+		self.ps.finishSolveStepByStep()
