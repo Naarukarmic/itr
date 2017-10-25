d6d2c93d7cf24565f50d1068181d4eb9210cbae5
Pierre Lemarquand <pierre.lemarquand@gmail.com>
2017-10-25 14:59:45 +0200

    ex2 hahahaha fuck off

--- td_concurrent/threadpool_src/lf_web.c
+++ td_concurrent/threadpool_src/lf_web.c
@@ -13,6 +13,15 @@
 
 #include "threadpool.h"
 
+/* Struct to pass as argument */
+typedef struct _webserver_args {
+  int sock;
+  struct sockaddr* from;
+  socklen_t* sin_len;
+} webserver_args;
+
+pthread_pool_t * thread_pool;
+
 char response[] = "HTTP/1.1 200 OK\r\n"
 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
 "<!DOCTYPE html><html><head><title>Mon serveur web</title>"
@@ -20,36 +29,36 @@ char response[] = "HTTP/1.1 200 OK\r\n"
 
 /******************************************************************************/
 void webserver_process (void *data) {
-
-  int *client_fd = (int *) data;
+  webserver_args* data_args = (webserver_args*) data;
   int bufsize = 1024;
   char *buffer = malloc(bufsize);
+  int client_fd  = accept(data_args->sock, data_args->from, data_args->sin_len);
+  pthread_pool_exec(thread_pool, webserver_process, data_args);
 
-  if (*client_fd == -1)
-    perror("accept");
+  if (client_fd == -1)
+    perror("accept");  
 
-  recv(*client_fd, buffer, bufsize, 0);
+  recv(client_fd, buffer, bufsize, 0);
   printf ("%s\n", buffer);
-  send(*client_fd, response, sizeof(response), 0);
-  close(*client_fd);
+  send(client_fd, response, sizeof(response), 0);
+  close(client_fd);
   free (buffer);
-
 }
 
 /******************************************************************************/
 int main(int argc, char **argv) {
-  pthread_pool_t *thread_pool = malloc (sizeof (thread_pool));
+  thread_pool = malloc (sizeof (thread_pool));
   int one = 1, client_fd;
   struct sockaddr_in svr_addr, from;
   socklen_t sin_len = sizeof(from);
+  
 
   /*--------------------------------------------------------------------------*/
   /* Initialize a thred pool of size 10 */
-  /* if (pthread_pool_init(10) < 0)
+  if ((thread_pool = pthread_pool_init(10)) < 0)
     perror ("ERROR: failed to create the thread pool, ");
-  */
-  thread_pool = pthread_pool_init(10);
   printf ("\x1B[32m %s \x1B[0m%s", "[MAIN]", "Start processing\n");
+
 
   /* Leave some time for the threadpool to finish its initialization */
   sleep (1);
@@ -76,11 +85,18 @@ int main(int argc, char **argv) {
 
   /*--------------------------------------------------------------------------*/
   /* Process incoming requests */
-  while (1) {
-    client_fd = accept(sock, (struct sockaddr *) &from, &sin_len);
-    pthread_pool_exec(thread_pool, 
-                     (void(*)()) webserver_process, 
-                     (void *)& client_fd);
+  webserver_args args;
+
+  args.sock = sock;
+  args.from = (struct sockaddr *) &from;
+  args.sin_len = &sin_len;
+
+  printf("Execution start.\n");
+  pthread_pool_exec(thread_pool, webserver_process, (void *) &args);
+  printf("Done executing.\n");
+
+  while (1) 
+  {
   }
 
   return EXIT_SUCCESS;
