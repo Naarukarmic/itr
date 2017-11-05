e5eca82a24b95d695703215e64c689c0dc932704
Pierre <pierre.lemarquand@gmail.com>
2017-11-05 07:51:49 -0500

    split tcp/udp methods

--- TraficGen/trafic_gen.c
+++ TraficGen/trafic_gen.c
@@ -1,8 +1,6 @@
 #include "isocket.h"
 
-#define BUF_SIZE 200
-
-void trafic_generator(char* host, int port, int proto_id, int packet_size){
+void udp_traffic(char* host, int port, int proto_id, int packet_size){
   int s, s2;
   char* proto;
 
@@ -12,28 +10,13 @@ void trafic_generator(char* host, int port, int proto_id, int packet_size){
     "<!DOCTYPE html><html><head><title>Mon serveur web</title>"
     "<body bgcolor=lightgreen><h1>Hello from my own web server !</h1></body></html>\r\n";
 
-  switch (proto_id) {
-    case 0: 
-      proto = "udp";
-      break;
-    case 1: 
-      proto = "tcp";
-      break;
+  s = i_socket_proto("udp");
+
+  if(i_bind(s, 1234) < 0) {
+    fprintf(stderr, "tr_gen bind failed\n");
+    exit(1);
   }
 
-  s = i_socket_proto(proto);
-
-  if (proto_id == 0) {
-    if(i_bind(s, 1234) < 0) {
-      fprintf(stderr, "tr_gen bind failed\n");
-      exit(1);
-    }
-  } else {
-    if(i_connect(s, host, port) < 0) {
-      fprintf(stderr, "connection refused\n");
-      exit(1);
-    }
-  }
   /*
   n = 0;
   while((cnt=read(s, buf, size)) > 0) {
@@ -42,9 +25,34 @@ void trafic_generator(char* host, int port, int proto_id, int packet_size){
     if(n >= 12) break;
   }
   close(s);
+  */
+}
 
-  
+void tcp_traffic(char* host, int port, int proto_id, int packet_size){
+  int s, s2;
+  char* proto;
 
+  char req[32] = "    GET / HTTP/1.1 \r\n";
+  char response[] = "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+    "<!DOCTYPE html><html><head><title>Mon serveur web</title>"
+    "<body bgcolor=lightgreen><h1>Hello from my own web server !</h1></body></html>\r\n";
+
+  s = i_socket_proto("tcp");
+
+  if(i_connect(s, host, port) < 0) {
+    fprintf(stderr, "connection refused\n");
+    exit(1);
+  }
+
+  /*
+  n = 0;
+  while((cnt=read(s, buf, size)) > 0) {
+    printf("%s\n", buf);
+    n += cnt;
+    if(n >= 12) break;
+  }
+  close(s);
   */
 }
 
@@ -65,6 +73,14 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  trafic_generator("localhost", 8080, atoi(argv[1]), atoi(argv[2]));
+  switch(atoi(argv[1])) {
+    case 0:
+      udp_traffic("localhost", 8080, atoi(argv[2]));
+      break;
+    case 1:
+      tcp_traffic("localhost", 8080, atoi(argv[2]));
+      break;
+  }
+
   return 0;
 }
