96ec15d56e5bebdbe1697ae8646cb81d2d8803d7
Pierre Lemarquand <pierre.lemarquand@gmail.com>
2017-11-28 16:41:01 +0100

    Added recursive first steps

--- BE RTSystems/be.py
+++ BE RTSystems/be.py
@@ -3,10 +3,96 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('xmlB1-periodique.xml')
 root = tree.getroot()
 
-# print root.tag, root.attrib
+elt = {} # Uniques dict
+mes = {} # Messages dict
+master = 'SXJJ'
 
-for child in root:
-    print child.tag, child.attrib
+for message in root:
+    tmp = {}
+    name = ''
+    for leaf in message:
+        # Building uniques dict
+        if elt.get(leaf.tag) is None:
+            elt[leaf.tag] = []
+        if leaf.text not in elt[leaf.tag]:
+            elt[leaf.tag].append(leaf.text)
+
+        # Building messages dict
+        if leaf.tag == 'nom':
+            name = leaf.text
+        else:
+            tmp[leaf.tag] = leaf.text
+    mes[name] = tmp
+
+# Total messages size
+for k,v in mes.iteritems():
+    mes_size = 0
+    if v['emetteur'] == master or v['recepteur'] == master:
+        mes_size += 56
+    else:
+        mes_size += 108
+    mes_size += int(v['taille_mes']) * 20
+    v['taille_tot'] = mes_size
+
+# Individual transmission delay
+# speed = 1000000.0 # bps -> delay in s
+speed = 1 # Mbps -> delay in us
+for k,v in mes.iteritems():
+    mes_size = v['taille_tot']
+    delay = mes_size / speed
+    v['trans_delay'] = delay
+
+# Transaction table
+freq = map(float, elt['frequence'])
+period = map(lambda x: 1/x, freq)
+micro = min(period)
+macro = max(period) # Technically LCM
+
+# Necessary condition for scheduling
+cond = 0
+for k,v in mes.iteritems():
+    cond += v['trans_delay']
+cond /= micro
+print 'CNS: ', cond, '<= 1'
+
+# WCRT
+# Divide messages per priority
+p = {'highest': {},
+     'high': {},
+     'low': {},
+     'lowest': {}}
+for k,v in mes.iteritems():
+    prio = float(v['frequence'])
+    if prio == 50: 
+        p['highest'][k] = [0, 0]
+    elif prio == 25:
+        p['high'][k] = [0, 0]
+    elif prio == 12.5:
+        p['low'][k] = [0, 0]
+    elif prio == 6.25:
+        p['lowest'][k] = [0, 0]
+
+# Then recursively calculate the WCRT
+def max_delay(d, d1=None, d2=None):
+    res = 0
+    for k,v in d.iteritems():
+        if mes[k]['trans_delay'] > res:
+            res = mes[k]['trans_delay']
+    if d1 is not None:
+        res = max(res, max_delay(d1))
+    if d2 is not None:
+        res = max(res, max_delay(d2))
+    return res
+
+for i in range(1000000):
+    for k,v in p['highest'].iteritems():
+        print k,v
+        v[1] = mes[k]['trans_delay']
+        v[1] += max_delay(p['high'], p['low'], p['lowest'])
+        
+
+# for k,v in mes.iteritems():
+#     print v
 
 # <message>
 #     <nom>DYYJ-0</nom>
