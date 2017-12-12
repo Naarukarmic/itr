324486dddb694f66df6a512afcfc72854618acda
Pierre Lemarquand <pierre.lemarquand@gmail.com>
2017-12-12 09:43:41 +0100

    Done periodic_task

--- IN_BE_Pump/periodic_task.c
+++ IN_BE_Pump/periodic_task.c
@@ -11,11 +11,27 @@ void *periodic_task_body (void *parameters);
 void *periodic_task_body (void *parameters) {
   struct timespec trigger;                      /* Stores next dispatch time */
   struct timespec period;                              /* Period of the task */
+  struct timespec now;
   sem_t timer;
   task_parameters *my_parameters = (task_parameters *) parameters;
+  int s;
 
   period = (*my_parameters).period;
+  if (sem_init(&timer, 0, 0) == -1) {
+    perror("sem_init");
+    exit(EXIT_FAILURE);
+  }
+  
+  clock_gettime(CLOCK_REALTIME, &now);
+  add_timespec(&trigger, &now, &period);
 
+  printf("main() about to call sem_timedwait()\n");
+  while((s = sem_timedwait(&timer, &trigger)) == -1) {
+    (*my_parameters).job();
+    continue;
+  }
+
+  exit((s == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
 }
 
 /*****************************************************************************/
@@ -26,6 +42,7 @@ void create_periodic_task (struct timespec period, void (*job) (void)) {
   parameters->period = period;
   parameters->job = job;
 
+  pthread_create(&tid, NULL, periodic_task_body, parameters);
 }
 /*****************************************************************************/
 #ifdef __TEST__
