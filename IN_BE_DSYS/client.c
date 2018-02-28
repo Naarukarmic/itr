a03d60b0c9000df0c91b49a04cc2c7795502753e
Pierre Lemarquand <pierre.lemarquand@gmail.com>
2018-02-28 14:45:01 +0100

    UDP server/client

--- IN_BE_DSYS/client.c
+++ IN_BE_DSYS/client.c
@@ -1,38 +1,49 @@
 /************* UDP CLIENT CODE *******************/
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char **argv){
+
+  if(argc != 2) {
+    printf("\n Use: ./client <id> \n");
+    return(1);
+  }
+
   int clientSocket, portNum, nBytes, v;
   char buffer[1024];
   char value[32];
-  char id[32];
+  char* id;
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
+
+  portNum = 7891;
 
   /*Create UDP socket*/
   clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
   /*Configure settings in address struct*/
   serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7891);
+  serverAddr.sin_port = htons(portNum);
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
   /*Initialize size variable to be used later on*/
   addr_size = sizeof serverAddr;
+  id = argv[1];
 
   while(1){
-    // TODO random
-    v = 12;
+    v = rand() % 100;
     sprintf(value, "%d", v);
-    strcpy(id, "1:");
 
     strcpy(buffer, id);
     strcat(buffer, value);
+    printf("%s",buffer);
     nBytes = strlen(buffer) + 1;
     
     /*Send message to server*/
--- IN_BE_DSYS/server.c
+++ IN_BE_DSYS/server.c
@@ -5,6 +5,9 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#define N_PROCESSUS 4
 
 int main(){
   int udpSocket, nBytes;
@@ -13,6 +16,19 @@ int main(){
   struct sockaddr_storage serverStorage;
   socklen_t addr_size, client_addr_size;
   int i;
+  int processusValues[N_PROCESSUS];
+
+  const char s[2]=":";
+  char *token;
+  int id;
+  int minimum = INT_MAX;
+  int old_minimum = INT_MAX;
+
+  /*Initiate processus values*/
+  for (i=0 ; i< N_PROCESSUS ; i++)
+  {
+    processusValues[i]=0;
+  }
 
   /*Create UDP socket*/
   udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
@@ -29,15 +45,43 @@ int main(){
   /*Initialize size variable to be used later on*/
   addr_size = sizeof serverStorage;
 
-  while(1){
-    /* Try to receive any incoming UDP datagram. Address and port of 
+  while(1)
+  {
+    /* Try to receive any incoming UDP datagram. Address and port of
       requesting client will be stored on serverStorage variable */
     nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
 
-    printf("%s\n", buffer);
+    /*Convert message received to uppercase*/
+    for(i=0;i<nBytes-1;i++)
+      buffer[i] = toupper(buffer[i]);
 
-    /*Send uppercase message back to client, using serverStorage as the address
-    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);*/
+    /*Parse the incoming string*/
+    token = strtok(buffer,s);
+    id = atoi(token);
+    while(token != NULL)
+    {
+        //printf("%s\n", token);
+        token = strtok(NULL,s);
+        if(token != NULL)
+        {
+            processusValues[id]=atoi(token);
+        }
+    }
+
+    /*Compute minimum*/
+    if(processusValues[id]< minimum)
+    {
+        minimum = processusValues[id];
+    }
+    
+    if(old_minimum!=minimum)
+    {
+        printf("Current minium : %d\n",minimum);
+    }
+    /*Send uppercase message back to client, using serverStorage as the address*/
+    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+
+    old_minimum = minimum;
   }
 
   return 0;
