cdf403340e13f2b93975d763159e7c6be166656e
Pierre Lemarquand <pierre.lemarquand@gmail.com>
2018-03-23 11:44:05 +0100

    Added food/exp

---
+++ IN_TP_SQL/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO nourriture VALUES
+(1, 'foin', 	'frogs'), 
+(2, 'foin', 	'california'),
+(3, 'fourrage', 'hopscotch'),
+(4, 'foin', 	'tonton'),
+(3, 'ble', 		'snapper'),
+(1, 'mais', 	'shanghai'),
+(5, 'mais', 	'basque');
+
+INSERT INTO exploitation VALUES
+(123456, 'isae',	 'GAEC'),
+(123126, 'ensica',	 'GAEC'),
+(121456, 'supaero',	 'EARL'),
+(128956, 'chill',	 'autre'),
+(114126, 'foyer', 	 'GAEC');
+
+INSERT INTO exp_nou_rel VALUES
+(123456, 'frogs'),
+(123456, 'california'),
+(123126, 'hopscotch'),
+(121456, 'tonton'),
+(121456, 'snapper'),
+(128956, 'shanghai'),
+(128956, 'basque'),
+(114126, 'hopscotch'),
+(114126, 'frogs'),
+(114126, 'snapper')
+;
+
+INSERT INTO vache VALUES
+(1289, 'john', 'limousine', 'bon', 45, troupeau_id)
+();
